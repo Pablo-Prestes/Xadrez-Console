420800f260e9ddc489d48d660547346a9b781517
Pablo <pablo.henrique.prestes@gmail.com>
2023-10-11 17:17:07 -0300

    Metodo para impressão do tabuleiro (Tela)

--- Xadrez-Console/Program.cs
+++ Xadrez-Console/Program.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using tabuleiro;
+using Xadrez_Console;
 
 namespace Xadrez {
     class Program {
         static void Main(string[] args) 
         {
             Tabuleiro tab = new Tabuleiro(8,8);
+            Tela.ImprimirTabuleiro(tab);
         }
     }
 }
---
+++ Xadrez-Console/Tela.cs
@@ -0,0 +1,27 @@
+﻿using tabuleiro;
+
+namespace Xadrez_Console
+{
+    class Tela
+    {
+        public static void ImprimirTabuleiro(Tabuleiro tabuleiro)
+        {
+            for (int i=0; i<tabuleiro.Linhas; i++)
+            {
+                for (int j = 0; j < tabuleiro.Colunas; j++)
+                {
+                    if (tabuleiro.peca(i,j) == null)
+                    {
+                        Console.Write("- ");
+                    }
+                    else 
+                    { 
+                    Console.Write(tabuleiro.peca(i,j) + " ");
+                    }
+                    
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
